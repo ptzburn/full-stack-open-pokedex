164d3ebbaafe314b93b315f8abcfaee7116e1185
ptzburn <milan.hokkanen@gmail.com>
2025-01-17 15:57:38 +0200

    added a new e2e test

--- e2e-tests/example.spec.js
+++ e2e-tests/example.spec.js
@@ -1,13 +1,19 @@
 const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Pokedex', () => {
-  test('front page can be opened', async ({ page }) => {
+  beforeEach(async ({ page }) => {
     await page.goto('/')
+  })
+
+  test('front page can be opened', async ({ page }) => {
     await expect(page.getByText('ivysaur')).toBeVisible()
-    await expect(
-      page.getByText(
-        'Pokémon and Pokémon character names are trademarks of Nintendo.'
-      )
-    ).toBeVisible()
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+
+  test('user can navigate from the main page to the Ivysaur page', async ({ page }) => {
+    const ivysaur = await page.getByText('ivysaur')
+    await ivysaur.click()
+    const chlorophyll = await page.getByText('chlorophyll')
+    await expect(chlorophyll).toBeVisible()
   })
 })
