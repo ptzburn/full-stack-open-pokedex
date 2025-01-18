9008110d8517e652d061a4ae1e9c42fc1824d7f5
ptzburn <milan.hokkanen@gmail.com>
2025-01-18 09:29:35 +0200

    added health checks

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5050
 
 app.use(express.static('dist'))
+
+app.get('/version', (req, res) => {
+  res.send('2') // change this string to ensure a new version deployed
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
